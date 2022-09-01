f3a93207b03dfdcb336426de869f5166791a228b
fadhilahm <fadhilahmetra@gmail.com>
2022-09-01 18:28:31 +0900

    Added slot-like feature.

--- starting-project/src/components/ExpenseItem.js
+++ starting-project/src/components/ExpenseItem.js
@@ -1,18 +1,23 @@
 import "./ExpenseItem.css";
+import Card from "./Card";
 
-const ExpenseItem = () => {
-  const expenseDate = new Date(1945, 3, 30);
-  const expenseTitle = "Messerschmitt-262A";
-  const expensePrice = 420.69;
+const ExpenseItem = (props) => {
+  const month = props.date.toLocaleString("en-us", { month: "long" });
+  const year = props.date.getFullYear();
+  const day = props.date.toLocaleString("en-us", { day: "2-digit" });
 
   return (
-    <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
-      <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">${expensePrice}</div>
+    <Card className="expense-item">
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
       </div>
-    </div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.price}</div>
+      </div>
+    </Card>
   );
 };
 
