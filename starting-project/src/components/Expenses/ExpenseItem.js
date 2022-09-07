f6bdecc636b04ef4270e546346f4cefce3b1df1a
fadhilahm <fadhilahmetra@gmail.com>
2022-09-07 18:33:02 +0900

    Added a form to add a new expense.

--- starting-project/src/components/Expenses/ExpenseItem.js
+++ starting-project/src/components/Expenses/ExpenseItem.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import "./ExpenseItem.css";
 import Card from "../UI/Card";
 
@@ -5,6 +7,13 @@ const ExpenseItem = (props) => {
   const month = props.date.toLocaleString("en-us", { month: "long" });
   const year = props.date.getFullYear();
   const day = props.date.toLocaleString("en-us", { day: "2-digit" });
+
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+    setTitle("Updated!");
+    console.log(title);
+  };
 
   return (
     <Card className="expense-item">
@@ -14,9 +23,10 @@ const ExpenseItem = (props) => {
         <div>{day}</div>
       </div>
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.price}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
